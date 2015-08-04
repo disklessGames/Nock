8f2f2093b4f1e7d7211dac2a99bec4b06d1fc0e5
Jannie Theron <jannie.theron@gmail.com>
2015-08-04 09:00:48 +0200

    New swift project file

---
+++ Nock/ViewController.swift
@@ -0,0 +1,25 @@
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Setup gamekit
+        
+        let gameView = SKView()
+        view = gameView
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
