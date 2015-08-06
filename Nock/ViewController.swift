c30bee27b230ef4c054e081de919a910d2cb2c1f
Jannie Theron <jannie.theron@gmail.com>
2015-08-07 00:16:05 +0200

    Upped minor version

--- Nock/ViewController.swift
+++ Nock/ViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 import SpriteKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,18 +17,26 @@ class ViewController: UIViewController {
         gameView.presentScene(scene)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func preferredInterfaceOrientationForPresentation() -> UIInterfaceOrientation {
         if view.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.Compact {
             return .Portrait
         } else {
-            return .Landscape
+            return .LandscapeLeft
         }
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return .Landscape
     }
 }
 
